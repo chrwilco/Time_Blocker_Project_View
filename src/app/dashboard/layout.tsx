0917662c6553c142cca46cffa6abe0cc23031603
Jake Dobler <jaecob.d1@gmail.com>
2024-01-24 18:08:06 -0500

    added shadcn, started working on dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,110 @@
+"use client";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import logo from "../../../public/timeblocker.png";
+import DarkModeToggle from "@/components/ui/dark-mode-button";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav className="w-full flex justify-between p-4 mr-24">
+        <Image src={logo} className="h-12 w-auto" alt="logo" />
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Todos</NavigationMenuTrigger>
+              <NavigationMenuContent className="flex flex-col">
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <a
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/"
+                      >
+                        {/* <Icons.logo className="h-6 w-6" /> */}
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          shadcn/ui
+                        </div>
+                        <p className="text-sm leading-tight text-muted-foreground">
+                          Beautifully designed components that you can copy and
+                          paste into your apps. Accessible. Customizable. Open
+                          Source.
+                        </p>
+                      </a>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem href="/docs" title="Introduction">
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </ListItem>
+                  <ListItem href="/docs/installation" title="Installation">
+                    How to install dependencies and structure your app.
+                  </ListItem>
+                  <ListItem
+                    href="/docs/primitives/typography"
+                    title="Typography"
+                  >
+                    Styles for headings, paragraphs, lists...etc
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
+              <NavigationMenuContent className="flex flex-col">
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  <NavigationMenuLink>Profile</NavigationMenuLink>
+                  <DarkModeToggle />
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="blank"></div>
+      </nav>
+
+      {children}
+    </section>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function DashboardPage() {
+  return (
+    <>
+      {/* <h1>Dashboard</h1> */}
+      {/* <Button color="primary">Click Me</Button> */}
+
+      {/* Show View of today's timeline */}
+      {/* Maybe with this
+      https://www.npmjs.com/package/@syncfusion/ej2-react-schedule
+      */}
+    </>
+  );
+}
+
+export default DashboardPage;
