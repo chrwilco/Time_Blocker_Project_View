7378ad7e3135884f93ee56cffcf301c0f5db90d7
Jake Dobler <jaecob.d1@gmail.com>
2024-01-26 22:02:31 -0500

    style changes

--- src/components/ui/dark-mode-button.tsx
+++ src/components/ui/dark-mode-button.tsx
@@ -5,6 +5,7 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import { NavigationMenuItem } from "./navigation-menu";
 
 export default function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
@@ -14,10 +15,10 @@ export default function DarkModeToggle() {
   };
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
+    <NavigationMenuItem onClick={toggleTheme} className="flex justify-start">
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
-    </Button>
+    </NavigationMenuItem>
   );
 }
--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -27,38 +27,36 @@ export default function Nav() {
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Todos</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Dashboard</NavigationMenuTrigger>
               <NavigationMenuContent className="flex flex-col">
                 <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
                     <NavigationMenuLink asChild>
-                      <a
+                      <Link
                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/profile"
+                        href="/dashboard"
                       >
                         {/* <Icons.logo className="h-6 w-6" /> */}
                         <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
+                          Dashboard
                         </div>
                         <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
+                          Add Breif glance of tasks.
                         </p>
-                      </a>
+                      </Link>
                     </NavigationMenuLink>
                   </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
+                  <ListItem href="/docs" title="Create taks">
+                    Create a daily, weekly, or monthly task.
                   </ListItem>
                   <ListItem href="/docs/installation" title="Installation">
                     How to install dependencies and structure your app.
                   </ListItem>
                   <ListItem
                     href="/docs/primitives/typography"
-                    title="Typography"
+                    title="Manage tasks"
                   >
-                    Styles for headings, paragraphs, lists...etc
+                    Manage your tasks.
                   </ListItem>
                 </ul>
               </NavigationMenuContent>
@@ -66,11 +64,13 @@ export default function Nav() {
             <NavigationMenuItem>
               <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
               <NavigationMenuContent className="flex flex-col">
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  <NavigationMenuLink>
-                    <Link href="/profile">Profile</Link>
-                  </NavigationMenuLink>
-                  <DarkModeToggle />
+                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
+                  <ListItem href="/profile" title="Profile">
+                    View and edit your profile.
+                  </ListItem>
+                  <ListItem>
+                    <DarkModeToggle />
+                  </ListItem>
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
